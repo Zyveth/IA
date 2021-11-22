71843f6634f1ac8eafd87ef3dd737204fd181de1
Zyveth <miguelasousamartins@gmail.com>
2021-11-22 18:45:18 +0000

    Queries 7 through 9.

---
+++ queries7_9.pl
@@ -0,0 +1,11 @@
+:-style_check(-discontiguous).
+:-style_check(-singleton).
+
+list([entrega(v,w,x,y,z,bicicleta),entrega(a,b,c,d,e,carro),entrega(f,g,h,i,j,moto)]).
+
+%================================
+% Query 1 - identificar o estafeta que utilizou mais vezes um meio de transporte mais
+% ecológico.
+%================================
+
+%query1():-.
