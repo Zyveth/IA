d50a8177a5e23b91569ae53a9c7db665599ac3d5
Zyveth <miguelasousamartins@gmail.com>
2021-11-22 19:36:32 +0000

    Pseudo-finish query 8.

--- queries7_9.pl
+++ queries7_9.pl
@@ -9,7 +9,7 @@
 %================================
 
 query7(Initial_Time/Final_Time,Entregas,bicicleta/X,moto/Y,carro/Z):-
-    filter_by_time(Initial_Time/Final_Time,Entregas,Time),
+    query8(Initial_Time/Final_Time,Entregas,Time),
     filter_transports(Time,T),
     count(T,bicicleta,X),
     count(T,moto,Y),
@@ -30,20 +30,6 @@ filter_transports([Entrega|Entregas],R):-
 
 get_transport(entrega(_,_,_,_,_,X),X).
 
-% Predicado que filtra as entregas guardando aquelas que ocurreram durante um intervalo de
-% de tempo específico.
-
-filter_by_time(_,[],[]).
-filter_by_time(Initial_Time/Final_Time,[Entrega|Entregas],[Entrega|T]):-
-    filter_by_time(Initial_Time/Final_Time,Entregas,Y),
-    get_time(Entrega,X),
-    X >= Initial_Time,
-    X =< Final_Time,
-    T = Y.
-filter_by_time(Initial_Time/Final_Time,[Entrega|Entregas],R):-
-    filter_by_time(Initial_Time/Final_Time,Entregas,Y),
-    R = Y.
-
 % Getter simples para obter a data na qual uma entrega foi feita.
 
 get_time(entrega(_,_,_,_,X,_),X).
@@ -58,3 +44,14 @@ count([X1|T],X,Z):- X1\=X,count(T,X,Z).
 % Query 8 - identificar o número total de entregas pelos estafetas, num determinado
 % intervalo de tempo;
 %================================
+
+query8(_,[],[]).
+query8(Initial_Time/Final_Time,[Entrega|Entregas],[Entrega|T]):-
+    query8(Initial_Time/Final_Time,Entregas,Y),
+    get_time(Entrega,X),
+    X >= Initial_Time,
+    X =< Final_Time,
+    T = Y.
+query8(Initial_Time/Final_Time,[Entrega|Entregas],R):-
+    query8(Initial_Time/Final_Time,Entregas,Y),
+    R = Y.
