35b838e087780af1190081a24112b4f84a18148e
Luis Fernandes <luistfernandes@gmail.com>
2021-11-26 21:48:06 +0000

    Adicionado ficheiro das queries 4 a 6

---
+++ queries4_6.pl
@@ -0,0 +1,28 @@
+:-style_check(-discontiguous).
+:-style_check(-singleton).
+
+:-use_module(library(lists)).
+
+:- include('Cliente.pl').
+:- include('Encomenda.pl').
+:- include('Entrega.pl').
+:- include('Estafeta.pl').
+
+%================================
+% Query 4 - 
+% 
+%================================
+
+
+
+%================================
+% Query 5 - 
+% 
+%================================
+
+
+
+%================================
+% Query 6 - 
+% 
+%================================
