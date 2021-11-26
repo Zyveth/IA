3c7a3a5bcfdd0317cd3a5bf42c44b674209ff1d2
Luis Fernandes <luistfernandes@gmail.com>
2021-11-26 23:41:22 +0000

    cenas a fazer nas queries 4 a 6

--- queries4_6.pl
+++ queries4_6.pl
@@ -9,20 +9,22 @@
 :- include('Estafeta.pl').
 
 %================================
-% Query 4 - 
-% 
+% Query 4 - identificar os clientes servidos por um determinado estafeta;
 %================================
 
-
-
-%================================
-% Query 5 - 
-% 
-%================================
-
-
+Entrega(idEstafeta, _, idEncomenda, _, _);
+Encomenda(idEncomenda, idCliente, _, _, _, _, _, _);    findall disto
 
 %================================
-% Query 6 - 
-% 
+% Query 5 - identificar quais as zonas (e.g., rua ou freguesia) com maior volume de entregas por parte da Green Distribution;
 %================================
+
+Cliente(IdCliente, Morada, Freguesia);
+Encomenda(idEncomenda, idCliente, _, _, _, _, _, _);
+Entrega(_, _, idEncomenda, _, _);                       somar isto e devolver a maior morada e maior freguesia
+
+%================================
+% Query 6 - calcular a classificação media de satisfação de cliente para um determinado estafeta; 
+%================================
+
+entrega(Estafeta, Nota, _, _, _). somar notas e dividir pelo n
