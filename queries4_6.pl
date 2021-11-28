c1c6eef566552e3b5915ef957b28c3c815bdf975
Luis Fernandes <luistfernandes@gmail.com>
2021-11-28 22:01:51 +0000

    Feita a query 6

--- queries4_6.pl
+++ queries4_6.pl
@@ -19,12 +19,31 @@ clientesServidosEstafeta(Estafeta,Clientes) :-
 % Query 5 - identificar quais as zonas (e.g., rua ou freguesia) com maior volume de entregas por parte da Green Distribution;
 %================================
 
-Cliente(IdCliente, Morada, Freguesia);
-Encomenda(idEncomenda, idCliente, _, _, _, _, _, _);
-Entrega(_, _, idEncomenda, _, _);                       somar isto e devolver a maior morada e maior freguesia
+%Cliente(IdCliente, Morada, Freguesia);
+%Encomenda(idEncomenda, idCliente, _, _, _, _, _, _);
+%Entrega(_, _, idEncomenda, _, _);                       somar isto e devolver a maior morada e maior freguesia
 
 %================================
 % Query 6 - calcular a classificação media de satisfação de cliente para um determinado estafeta; 
 %================================
 
-entrega(Estafeta, Nota, _, _, _). somar notas e dividir pelo n
+classificacoesEstafeta(Estafeta,Classificacoes) :-
+    findall(Classificação,entrega(Estafeta,Classificação,_,_,_),Classificacoes).
+
+length([], 0).
+length([H|T], N) :- length(T, N1), N is N1+1.
+
+sum([],0).
+sum([X|List],Sum) :-
+    sum(List,Sum1),
+    Sum = X + Sum1.
+
+mediaLista(Lista,Media) :-
+    length(Lista,N), sum(Lista,Soma),
+    Media is Soma/N.
+
+classificacaoMediaEstafeta(Estafeta,Media) :-
+    classificacoesEstafeta(Estafeta,Classificacoes),
+    mediaLista(Classificacoes,Media).
+
+
