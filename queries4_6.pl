fd51e78dacba6c239080ca6ba64f5b3a84a14c27
Luis Fernandes <luistfernandes@gmail.com>
2021-11-30 11:37:08 +0000

    query 5 feitas as listas | falta contar e devolver o mais frequente

--- queries4_6.pl
+++ queries4_6.pl
@@ -19,9 +19,11 @@ clientesServidosEstafeta(Estafeta,Clientes) :-
 % Query 5 - identificar quais as zonas (e.g., rua ou freguesia) com maior volume de entregas por parte da Green Distribution;
 %================================
 
-%Cliente(IdCliente, Morada, Freguesia);
-%Encomenda(idEncomenda, idCliente, _, _, _, _, _, _);
-%Entrega(_, _, idEncomenda, _, _);                       somar isto e devolver a maior morada e maior freguesia
+ruasTodasEntregas(Ruas) :-
+    findall(Morada,(cliente(IdCliente, Morada, _),encomenda(IdEncomenda, IdCliente, _, _, _, _, _, _),entrega(_, _, IdEncomenda, _, _)),Ruas).
+
+freguesiasTodasEntregas(Freguesias) :-
+    findall(Feguesia,(cliente(IdCliente, _, Freguesia),encomenda(IdEncomenda, IdCliente, _, _, _, _, _, _),entrega(_, _, IdEncomenda, _, _)),Freguesias).                      somar isto e devolver a maior morada e maior freguesia
 
 %================================
 % Query 6 - calcular a classificação media de satisfação de cliente para um determinado estafeta; 
@@ -30,8 +32,8 @@ clientesServidosEstafeta(Estafeta,Clientes) :-
 classificacoesEstafeta(Estafeta,Classificacoes) :-
     findall(Classificação,entrega(Estafeta,Classificação,_,_,_),Classificacoes).
 
-length([], 0).
-length([H|T], N) :- length(T, N1), N is N1+1.
+length2([], 0).
+length2([H|T], N) :- length(T, N1), N is N1+1.
 
 sum([],0).
 sum([X|List],Sum) :-
@@ -39,7 +41,7 @@ sum([X|List],Sum) :-
     Sum = X + Sum1.
 
 mediaLista(Lista,Media) :-
-    length(Lista,N), sum(Lista,Soma),
+    length2(Lista,N), sum(Lista,Soma),
     Media is Soma/N.
 
 classificacaoMediaEstafeta(Estafeta,Media) :-
