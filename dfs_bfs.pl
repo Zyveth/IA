43826be8d2a3e07d05c2d79443839abb52356d30
Luis Fernandes <luistfernandes@gmail.com>
2022-01-04 23:50:29 +0000

    bilp a funcionar?

--- dfs_bfs.pl
+++ dfs_bfs.pl
@@ -12,7 +12,7 @@ dfs(Destino,Caminho):-
     origem(Origem),
     dfsAux(Origem,Destino,[Origem],Caminho).
 
-%condicao paragem qd vertice atual e destino sao iguais. Basta inverter caminho #TODO isto é só caminho de ida
+%condicao paragem qd vertice atual e destino sao iguais. Basta inverter caminho
 dfsAux(Destino,Destino,[H|T],Caminho):-
     reverse([H|T],Cam),
     append(Cam,T,Caminho).
@@ -23,19 +23,19 @@ dfsAux(Actual,Destino,LA,Caminho):-
     dfsAux(X,Destino,[X|LA],Caminho). %chamada recursiva
 
 %%%%%%%%%%%%%%%%%%%%%
-%DFS MAIS Q 1 ENTREGA
+%DFS MAIS Q 1 ENTREGA #TODO
 %%%%%%%%%%%%%%%%%%%%%
 
 dfsV(Destinos,Caminho):-
     origem(Origem),
     dfsVAux(Origem,Destinos,[Origem],Caminho).
 
-% fucking hell
+% condicao de paragem qd deixa de haver destinos na lista
 dfsVAux(Destino,[],[H|T],Caminho):-
     reverse([H|T],Cam),
     append(Cam,T,Caminho).
   
-%condicao paragem qd vertice atual e destino sao iguais. Basta inverter caminho #TODO isto é só caminho de ida
+%condicao paragem qd vertice atual e destino sao iguais. Basta inverter caminho
 dfsVAux(Destino,[Destino],[H|T],Caminho):-
     reverse([H|T],Cam),
     append(Cam,T,Caminho).
@@ -64,3 +64,45 @@ bfsAux(Destino,[LA|Outros],Caminho):-
     findall([X|LA],(Destino\==Actual,ligacao(Actual,X),\+ member(X,LA)),Novos), % calcular todos os vertices adjacentes não visitados e gerar um caminho novo c/ cada vertice e caminho actual
     append(Outros,Novos,Todos), %caminhos novos são colocados no final da lista
     bfsAux(Destino,Todos,Caminho). %chamada recursiva
+
+%%%%%%%%%%%%%%%%%%%%%
+%BFS MAIS Q 1 ENTREGA #TODO
+%%%%%%%%%%%%%%%%%%%%%
+
+bfsV(Destinos,Caminho):-
+    origem(Origem),
+    bfsVAux(Destinos,[[Origem]],Caminho).
+
+%fucking hell
+bfsVAux([],[[Destino|T]|_],Caminho):-
+    reverse([Destino|T],Cam),
+    append(Cam,T,Caminho).
+
+%condicao paragem: qd destino = vertice à cabeça do caminho actual. Basta inverter caminho
+bfsVAux([Destino],[[Destino|T]|_],Caminho):-
+    reverse([Destino|T],Cam),
+    append(Cam,T,Caminho).
+
+bfsVAux(Dest,[LA|Outros],Destinos):-
+    LA=[Actual|_],
+    findall([X|LA],(ligacao(Actual,X),\+ member(X,LA)),Novos), % calcular todos os vertices adjacentes não visitados e gerar um caminho novo c/ cada vertice e caminho actual
+    append(Outros,Novos,[[H|_]|_]), %caminhos novos são colocados no final da lista
+    delete(Dest,H,AA).%#TODO isto está mal eu n posso apagar destinos 
+    bfsVAux(Destinos,Todos,Caminho). %chamada recursiva
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%Busca Iterativa Limitada em Profundidade          %TODO falta fazer o caminho total e perceber direito o cut
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+bilp(GoalNode, Max, Solution) :- 
+    origem(Origin),
+    path(Origin, GoalNode, Solution),
+    length(Solution, Length),
+    ( (Length =< Max) ; (Length > Max), !, fail).
+
+path(Node, Node, [Node]).
+
+path(FirstNode, LastNode, [LastNode|Path]) :- 
+    path(FirstNode, OneButLast, Path),
+    ligacao(OneButLast, LastNode),
+    not(member(LastNode, Path)).
