937ace8a37a5ba751dfcc4fa43874eda8647cfbc
Zyveth <miguelasousamartins@gmail.com>
2022-01-07 20:14:49 +0000

    Atualizaçao

--- calcula.pl
+++ calcula.pl
@@ -1,7 +1,20 @@
+% Calcula tempos para 2ª fase do projeto de Inteligência Artificial 2021/22
+% 2022/01/01
+
+% Style Check
+
+:- style_check(-discontiguous).
+:- style_check(-singleton).
+
+% Includes
+
 :- include('dfs_bfs_bilp.pl').
 :- include('Cliente.pl').
 :- include('Encomenda.pl').
+:- include('aestrela.pl').
+:- include('gulosa.pl').
 
+% Dfs
 
 calculaTempo("dfs", Encomendas, "Carro", Caminho/Tempo) :-
     calculaPesoTotal(Encomendas,Peso),
@@ -10,7 +23,103 @@ calculaTempo("dfs", Encomendas, "Carro", Caminho/Tempo) :-
     dfs(1,Rota,Caminho/Custo),
     Velocidade is 25 - (0.1*Peso),
     Tempo is Custo/Velocidade.
+
+calculaTempo("dfs", Encomendas, "Mota", Caminho/Tempo) :-
+    calculaPesoTotal(Encomendas,Peso),
+    Peso =< 20,
+    calculaParagens(Encomendas,Rota),
+    dfs(1,Rota,Caminho/Custo),
+    Velocidade is 35 - (0.5*Peso),
+    Tempo is Custo/Velocidade.
+
+calculaTempo("dfs", Encomendas, "Bicicleta", Caminho/Tempo) :-
+    calculaPesoTotal(Encomendas,Peso),
+    Peso =< 5,
+    calculaParagens(Encomendas,Rota),
+    dfs(1,Rota,Caminho/Custo),
+    Velocidade is 10 - (0.7*Peso),
+    Tempo is Custo/Velocidade.
+
+%Bfs
+
+calculaTempo("bfs", Encomendas, "Carro", Caminho/Tempo) :-
+    calculaPesoTotal(Encomendas,Peso),
+    Peso =< 100,
+    calculaParagens(Encomendas,Rota),
+    bfs(1,Rota,Caminho/Custo),
+    Velocidade is 25 - (0.1*Peso),
+    Tempo is Custo/Velocidade.
+
+calculaTempo("bfs", Encomendas, "Mota", Caminho/Tempo) :-
+    calculaPesoTotal(Encomendas,Peso),
+    Peso =< 20,
+    calculaParagens(Encomendas,Rota),
+    bfs(1,Rota,Caminho/Custo),
+    Velocidade is 35 - (0.5*Peso),
+    Tempo is Custo/Velocidade.
+
+calculaTempo("bfs", Encomendas, "Bicicleta", Caminho/Tempo) :-
+    calculaPesoTotal(Encomendas,Peso),
+    Peso =< 5,
+    calculaParagens(Encomendas,Rota),
+    bfs(1,Rota,Caminho/Custo),
+    Velocidade is 10 - (0.7*Peso),
+    Tempo is Custo/Velocidade.
     
+% AEstrela
+
+calculaTempo("aestrela", Encomendas, "Carro", Caminho/Tempo) :-
+    calculaPesoTotal(Encomendas,Peso),
+    Peso =< 100,
+    calculaParagens(Encomendas,Rota),
+    aestrela(1,Rota,Caminho/Custo),
+    Velocidade is 25 - (0.1*Peso),
+    Tempo is Custo/Velocidade.
+
+calculaTempo("aestrela", Encomendas, "Mota", Caminho/Tempo) :-
+    calculaPesoTotal(Encomendas,Peso),
+    Peso =< 20,
+    calculaParagens(Encomendas,Rota),
+    aestrela(1,Rota,Caminho/Custo),
+    Velocidade is 35 - (0.5*Peso),
+    Tempo is Custo/Velocidade.
+
+calculaTempo("aestrela", Encomendas, "Bicicleta", Caminho/Tempo) :-
+    calculaPesoTotal(Encomendas,Peso),
+    Peso =< 5,
+    calculaParagens(Encomendas,Rota),
+    aestrela(1,Rota,Caminho/Custo),
+    Velocidade is 10 - (0.7*Peso),
+    Tempo is Custo/Velocidade.
+
+% Gulosa
+
+calculaTempo("gulosa", Encomendas, "Carro", Caminho/Tempo) :-
+    calculaPesoTotal(Encomendas,Peso),
+    Peso =< 100,
+    calculaParagens(Encomendas,Rota),
+    gulosa(1,Rota,Caminho/Custo),
+    Velocidade is 25 - (0.1*Peso),
+    Tempo is Custo/Velocidade.
+
+calculaTempo("gulosa", Encomendas, "Mota", Caminho/Tempo) :-
+    calculaPesoTotal(Encomendas,Peso),
+    Peso =< 20,
+    calculaParagens(Encomendas,Rota),
+    gulosa(1,Rota,Caminho/Custo),
+    Velocidade is 35 - (0.5*Peso),
+    Tempo is Custo/Velocidade.
+
+calculaTempo("gulosa", Encomendas, "Bicicleta", Caminho/Tempo) :-
+    calculaPesoTotal(Encomendas,Peso),
+    Peso =< 5,
+    calculaParagens(Encomendas,Rota),
+    gulosa(1,Rota,Caminho/Custo),
+    Velocidade is 10 - (0.7*Peso),
+    Tempo is Custo/Velocidade.
+
+
+
 calculaParagens([Encomenda], Rota):-
     encomenda(Encomenda, IDCliente, _, _, _, _, _, _),
     cliente(IDCliente,Morada,_),
